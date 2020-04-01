bf85657609c5a0d8fbdeaafff7843624f1f86428
Nikola Nedeljkovic <nikola.b.nedeljkovic@gmail.com>
2020-04-01 10:24:43 +0200

    Fixing pom, and imports

--- src/main/java/com/nikolanedeljkovic/flightadvisor/service/UserDetailsServiceImpl.java
+++ src/main/java/com/nikolanedeljkovic/flightadvisor/service/UserDetailsServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.nikolanedeljkovic.flightadvisor.repository.UserRepository;
-import com.nikolanedeljkovic.flightadvisor.user.User;
+import com.nikolanedeljkovic.flightadvisor.domain.user.User;
 
 
 @Service
